592c946fe32a0212d923f0299a0b710b55ac9fcc
celescript <celestefernandez00@gmail.com>
2023-10-31 17:26:53 -0300

    changing things

--- components/organisms/hero/title.tsx
+++ components/organisms/hero/title.tsx
@@ -3,9 +3,12 @@ export const Title = () => {
 
   return (
     <div className='overflow-hidden'>
-      <h1 className='font-medium font-mono leading-snug text-white text-center text-2xl md:text-7xl translate-y-[100px] clip'>
-        Hi, I{`'`}m{' '}
-        <span className='bg-clip-text font-sans inline-block font-bold text-transparent bg-gradient-to-r from-orange-300  via-pink-500 to-sky-400 bg-animation '>
+      <h1 className='font-medium font-mono leading-snug text-white text-center text-2xl lg:text-7xl   '>
+        <span className='clip translate-y-[150px] inline-block'>
+          Hi, I{`'`}m{' '}
+        </span>{' '}
+        <br />
+        <span className='bg-clip-text translate-y-[100px] font-sans inline-block font-bold text-transparent bg-gradient-to-r from-orange-300  via-pink-500 to-sky-400 bg-animation clip'>
           {text}
         </span>
       </h1>
