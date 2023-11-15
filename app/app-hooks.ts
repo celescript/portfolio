14d0a9f15c99578b979f1d7bd4645d5b7f56bbf5
celescript <celestefernandez00@gmail.com>
2023-11-15 13:46:11 -0300

    testing configs

--- app/app-hooks.ts
+++ app/app-hooks.ts
@@ -4,12 +4,8 @@ import { useScroll } from '@/utils/hooks/use-scroll'
 
 export const AppHooks = () => {
   useScroll({
-    // lerp: 0.5,
-    smoothWheel: true,
-    normalizeWheel: true,
-    touchMultiplier: 1.2,
+    touchMultiplier: 0.8,
     wheelMultiplier: 0.8,
-    smoothTouch: true,
   })
 
   return null
